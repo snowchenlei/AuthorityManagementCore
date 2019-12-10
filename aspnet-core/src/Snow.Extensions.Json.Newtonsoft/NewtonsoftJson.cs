6f2c54cce109b2b0e39ddb091c0f0bc9aa2890e9
陈磊 <gbdvjxu1257q@outlook.com>
2019-12-10 15:04:53 +0800

    修复bug

---
+++ aspnet-core/src/Snow.Extensions.Json.Newtonsoft/NewtonsoftJson.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using Snow.Extensions.Json.Abstractions;
+
+namespace Snow.Extensions.Json.Newtonsoft
+{
+    public class NewtonsoftJson : IJson
+    {
+        public object Deserialize(string json)
+        {
+            return JsonConvert.DeserializeObject(json);
+        }
+
+        public TModel Deserialize<TModel>(string json)
+        {
+            return JsonConvert.DeserializeObject<TModel>(json);
+        }
+
+        public string Serialize(object model)
+        {
+            return JsonConvert.SerializeObject(model);
+        }
+
+        public string Serialize<TModel>(TModel model)
+        {
+            return JsonConvert.SerializeObject(model);
+        }
+    }
+}
